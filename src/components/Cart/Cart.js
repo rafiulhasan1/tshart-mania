d9905ff39351b5d183092fe0df5dbed414ae45d5
rafiulhasan1 <rafiulhasan001@gmail.com>
2022-10-09 21:48:20 +0600

    last commit

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
 const Cart = ({cart ,removeToCart }) => {
+
+    let message;
+    if(cart.length === 0){
+        message = <p>no product seclated</p>
+    }
+    else{
+        message =<p>Thanks For Buying</p>
+    }
+
     return (
         <div>
             <h2>products Detail</h2>
@@ -13,6 +22,11 @@ const Cart = ({cart ,removeToCart }) => {
                     <button onClick={() => removeToCart(tshirt)}>X</button>
                 </p>)
             }
+            {
+                message
+            }
+            {cart.length === 3 ? <p> tin jon k gift diba</p> : <p>assa kino</p>}
+            {cart.length ==2 && <p>Dobul Item</p>}
         </div>
     );
 };
